a70f8b4ab39c6ecff31895db457c3a6a32587207
Caio-Castilho <caio.aspira2000@gmail.com>
2022-08-05 19:46:39 -0300

    adição de fonte personalizada

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         {/* <link rel="stylesheet" href="styles/styles.scss"/> */}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-      {/* <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet"/> */}
+        <link href="https://fonts.googleapis.com/css2?family=Gentium+Book+Plus:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
       </Head>
       <body>
         <Main />
